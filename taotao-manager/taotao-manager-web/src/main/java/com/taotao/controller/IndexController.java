5273ff0c4f984b75a43872ae22e686f6163b9869
耿远博 <gyb_cloud@163.com>
2018-03-24 10:55:43 +0800

    修改IndexController.java

--- taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/IndexController.java
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.taotao.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,5 +10,10 @@ public class IndexController {
 	@RequestMapping(value= {"","/"})
 	public String index() {
 		return "index";
+	}
+	
+	@RequestMapping("/{page}")
+	public String page(@PathVariable String page) {
+		return page;
 	}
 }
